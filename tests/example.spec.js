dc924d39d062effaacce1b5c338e2f187b438a2f
manianbu5700 <manianbu5700@gmail.com>
2026-09-15 10:05:44 +0530

    file modified

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -3,7 +3,10 @@ import { test } from '@playwright/test'
 
 test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
+});
 
+test('validate flipkart', async ({ page }) => {
+  await page.goto('https://flipkart.com/');
 });
 
 

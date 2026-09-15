da825fbf657200d0fa3ef1dcb4a74bb4330637a1
manianbu5700 <manianbu5700@gmail.com>
2026-09-15 10:09:07 +0530

    file modified

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -3,7 +3,10 @@ import { test } from '@playwright/test'
 
 test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
+});
 
+test('validate amazon application', async ({ page }) => {
+  await page.goto('https://amazon.in/');
 });
 
 
